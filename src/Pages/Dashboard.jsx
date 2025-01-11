d3d33ac705afca67c425844902274d82da453a60
Jalaj Sharma <jalajsharma172@gmail.com>
2025-01-11 22:52:54 +0530

    Initial commit

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -12,11 +12,40 @@ const Dashboard = () => {
         <div className="manu">
           <p className="manu-head ">Start your journey by exploring our community spaces.</p>
           <div className="account">
-            <Link to="/create/Manu_account" className ='Manu-acc'>Create Manufactuere Account</Link>
-            <Link to="/create/User_account" className ='User-acc'>Create user Account</Link>
+      
+            <Link to="/create/Manu_account" className ='Manu-acc'>Create Manufacturer Account</Link>
+            <Link to="/create/User_account" className ='User-acc'>Create User Account</Link>
             </div>
         </div>
+        <div class="container">
+        <h1>How It Works</h1>
+        <div class="steps">
+            <div class="step step-1">
+                <div class="icon">👥</div>
+                <h2>Step 1</h2>
+                <p>Create a Space</p>
+                <p>Create a SpaceCreate a SpaceCreate a SpaceCreate a SpaceCreate a SpaceCreate a Space</p>
+            </div>
+            {/* <div class="step step-2">
+                <div class="icon">🔒</div>
+                <h2>Step 2</h2>
+                <p>Setup Market</p>
+                <p>Setup MarketSetup MarketSetup MarketSetup MarketSetup MarketSetup MarketSetup Market</p>
+            </div> */}
+            {/* <div class="step step-3">
+                <div class="icon">🔒</div>
+                <h2>Step 2</h2>
+                <p>Setup Market</p>
+                <p>Setup MarketSetup MarketSetup MarketSetup MarketSetup MarketSetup MarketSetup Market</p>
+            </div> */}
+       
+        </div>
       </div>
+
+   
+    </div>
+
+      
     </div>  
   );
 };
